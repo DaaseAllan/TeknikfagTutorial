f167949f4a096a19ae7b853b19116e81d03b5ff7
DaaseAllan <laurits.mumberg@gmail.com>
2018-09-20 13:44:25 +0200

    Camera follow uden at dreje

---
+++ TeknikfagTutorial/Assets/_Complete-Game/Scripts/CameraFollowSCript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowSCript : MonoBehaviour {
+    public GameObject playerObject;
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 cameraPos = new Vector3(playerObject.transform.position.x + xOffset, playerObject.transform.position.y + yOffset, playerObject.transform.position.z + zOffset);
+        transform.position = cameraPos;
+	}
+}
